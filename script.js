c31dbf1ce75ac86387992ec29117f671e7140426
Levalleur Natacha <levalleur.natacha@gmail.com>
2024-07-08 15:20:39 +0200

    ajout des fleches html et event listener javascript

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./images/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./images/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./images/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./images/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log (slides)
+
+// variable fleches slider
+
+const arrowLeft = document.querySelector ( ".arrow_left")
+const arrowRight = document.querySelector (".arrow_right")
+
+// fonction clique fleches slider 
+
+function clickArrowLeft () {
+	arrowLeft.addEventListener ("click",() => {
+	console.log ("gauche");
+	});
+}
+clickArrowLeft ();
+
+function clickArrowRight() {
+	arrowRight.addEventListener ("click",()=> {
+		console.log ("droite");
+
+	
+});
+}
+clickArrowRight ();
