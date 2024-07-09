04ea3efdc696729a8bb987316a581ecd3d98b3d1
Levalleur Natacha <levalleur.natacha@gmail.com>
2024-07-09 14:32:05 +0200

    ajout image slider et modification des fonction des bullet points

--- script.js
+++ script.js
@@ -16,42 +16,85 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-console.log (slides)
 
 // variable fleches slider
 
-const arrowLeft = document.querySelector ( ".arrow_left")
-const arrowRight = document.querySelector (".arrow_right")
+const arrowLeft = document.querySelector ( ".arrow_left");
+const arrowRight = document.querySelector (".arrow_right");
 
 // variable Bullet points
 
-const dots = document.querySelector (".dots")
+const dots = document.querySelector (".dots");
+const pointsPleins= document.querySelector (".arrowRight");
+
+let index = 0
+
+//variable des images et texte de la banniere
+const banner= document.getElementById ("banner")
+const bannerImage=banner.querySelector (".banner-img");
+const bannerText= banner.querySelector("p")
 
 // fonction clique fleches slider 
+//gauche
 
 function clickArrowLeft () {
 	arrowLeft.addEventListener ("click",() => {
-	console.log ("gauche");
-	});
+		//fonction changement d'image au click gauche
+		index --;
+			if (index < 0) {
+				index = slides - 1 ;
+			}
+		// modification image et texte
+		bannerImage.src = slides[index].image;
+		bannerText.innerHTML = slides[index].tagLine;
+		
+	})
 }
 clickArrowLeft ();
 
-function clickArrowRight() {
-	arrowRight.addEventListener ("click",()=> {
-		console.log ("droite");
+//droite
 
-	
-});
+function clickArrowRight() {
+	arrowRight.addEventListener ( "click",() => {
+
+		// retrait de la class css des bullet pleins
+		const arrayDots = document.querySelectorAll (".dots .dot");
+		arrayDots[index].classList.remove ("dot_selected");
+		
+		//fonction changement d'image au click droit
+		index ++;
+			if (index > slides.length - 1 ){
+				index = 0
+			}
+		
+		
+		//ajout des differentes images et texte au slider
+		bannerImage.src = slides[index].image;
+		bannerText.innerHTML = slides[index].tagLine;
+		// ajoutu de la class css des bullet pleins lorsque l'image à changé
+		arrayDots[index].classList.add ("dot_selected");
+		
+	})
 }
 clickArrowRight ();
+
 
 //fonction bullet points
 
 function displayDots() {
 	for (let i =0 ; i< slides.length; i++){
-		const dot=document.createElement ("div")
+		const dot=document.createElement ("div");
 		dot.classList.add ("dot");
 		dots.appendChild (dot);
+		if (i == index) {
+		dot.classList.add("dot_selected");
+		}
+		dot.addEventListener ("click", () =>{
+			index = i;
+			clickArrowRight();	
+			
+		})
 	}
 }
 displayDots();
+
